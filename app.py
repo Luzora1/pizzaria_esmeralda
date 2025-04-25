ba59cac03cd11028a4d6d4e5ce5f632efb8617e2
dylan adriel <dylanadriel25@gmail.com>
2025-04-25 16:43:03 -0300

    Adicionando area do adm e fazendo algumas melhorias

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, session, redirect, url_for, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import date
 
 ##########################
 # atenção: O session é um dicionário que armazena os dados do usuário logado, ele é armazenado no navegador do usuário e é usado para manter o estado da sessão entre as requisições.
@@ -11,7 +12,10 @@ app.secret_key = "sua_chave_secreta"
 
 
 # Banco falso esse aqui, só para teste
-usuarios = {'emailteste@email.com': {'nome': 'ADM', 'senha': 'scrypt:32768:8:1$qz4eXywBRQIcyaTs$c6e561005157ca7f657f3f716f395eaea8a5ff9b36879cde0f22ec6e1a5422e17ac53c4ad123f02ac1b929cfc6d750c6e9024aa56c6c26b5fd142322f02c5e85', 'xp': 3000, 'esmeraldas': 50, 'endereco': 'Rua teste, 123','email': 'emailteste@email.com'}}
+usuarios = {'emailteste@email.com': {'nome': 'Eumesmo', 'senha': 'scrypt:32768:8:1$qz4eXywBRQIcyaTs$c6e561005157ca7f657f3f716f395eaea8a5ff9b36879cde0f22ec6e1a5422e17ac53c4ad123f02ac1b929cfc6d750c6e9024aa56c6c26b5fd142322f02c5e85', 'xp': 3000, 'esmeraldas': 50, 'endereco': 'Rua teste, 123','email': 'emailteste@email.com'}}
+admin = {'ADM-1': {'senha': 'scrypt:32768:8:1$qz4eXywBRQIcyaTs$c6e561005157ca7f657f3f716f395eaea8a5ff9b36879cde0f22ec6e1a5422e17ac53c4ad123f02ac1b929cfc6d750c6e9024aa56c6c26b5fd142322f02c5e85'}}
+pedidos = {} 
+id_contador  = 1
 
 @app.route('/')
 def loginRegister():
@@ -64,11 +68,15 @@ def logout():
 @app.route('/home')
 def home():
 
-
+    if 'user' not in session:
+        return redirect('/')
+    
     user = session.get('user')
 
-    level = user["xp"] // 100
-
+    level = 0
+    if user and 'xp' in user:
+        level = user['xp'] // 100
+        
     desconto = 0
 
     if level >=5:
@@ -153,6 +161,9 @@ def adicionar_carrinho():
 
 @app.route('/carrinho')
 def carrinho():
+    if 'user' not in session:
+        return redirect('/')
+    
     user = session.get('user')
     if "carrinho" not in session:
         session["carrinho"] = []
@@ -189,17 +200,35 @@ def deletar_item():
 
 @app.route('/pagar', methods=['POST'])
 def pagar():
+    global id_contador  # Para acessar e modificar a variável global
     data = request.get_json()
     total = data.get('total')
 
     if 'user' in session and session['user']['esmeraldas'] >= total:
-        session['user']['esmeraldas'] -= total # Atualiza o saldo do usuário 
-        # Aqui voce deve fazer a atualização no banco de dados real, esse só está sendo usado pq nao temos um banco de dados real
-        # Lembrar de adicionar o endereço do usuário no pedido
-        # Aqui voce deve adicionar o pedido no banco de dados real, esse só está sendo usado pq nao temos um banco de dados real, o pedido está no session["carrinho"]
-        session["carrinho"] = [] # Limpa o carrinho após o pagamento
-        session['user']['xp'] += 55 # Adiciona XP ao usuário, aqui você pode adicionar o valor que quiser, só para teste, atualizar no BD real 
-        session.modified = True  
+        session['user']['esmeraldas'] -= total
+
+        carrinho = session.get("carrinho", [])
+        # criando o pedido com os dados do carrinho e do usuario
+        pedido = {
+            "pizzas": [i[0] for i in carrinho],
+            "preco": total,
+            "endereco": session['user']['endereco'],
+            "email": session['user']['email'],
+            "data": str(date.today()),
+            "status": "Pendente"
+        }
+        # Adicionando o pedido ao "banco de dados" 
+        # Aqui você pode adicionar o pedido ao banco de dados real, esse só está sendo usado pq nao temos um banco de dados real
+        pedidos[id_contador] = pedido       
+        id_contador += 1 
+        print(pedidos)
+
+        # Limpa o carrinho após o pagamento
+        session["carrinho"] = []
+        # Atualiza o XP do usuário
+        session['user']['xp'] += 55
+        session.modified = True
+
         return jsonify({
             'mensagem': 'Pagamento realizado com sucesso!',
             'novo_saldo': session['user']['esmeraldas']
@@ -208,8 +237,12 @@ def pagar():
         return jsonify({'mensagem': 'Saldo insuficiente ou usuário não logado.'}), 400
 
 
+
 @app.route('/user')
 def user():
+    if 'user' not in session:
+        return redirect('/')
+    
     user = session.get('user')
 
     return render_template("user.html", user=user)
@@ -233,6 +266,9 @@ def userEdit():
 
 @app.route('/shop')
 def shop():
+    if 'user' not in session:
+        return redirect('/')
+    
     user = session.get('user')
 
     return render_template("shop.html", user=user)
@@ -241,16 +277,81 @@ def shop():
 @app.route('/buyEsmeraldas', methods=['POST'])
 def buyEsmeraldas():
     # Pegando os dados do form
+    quantidade = int(request.form['quantidade'])
     user = session.get('user')
     # Atualizando o user no session, aqui voce tem que pegar as modificações e atualizar no banco de dados real, esse só está sendo usado pq nao temos um banco de dados real
-    session['user']['esmeraldas'] += 100 
-    session['user']['xp'] += 15 
+    session['user']['esmeraldas'] += quantidade 
+    session['user']['xp'] += quantidade // 5 
     session.modified = True  
     user = session.get('user')
 
     return render_template("shop.html", user=user)
 
 
+@app.route('/loginAdmin')
+def loginAdmin():
+    return render_template('loginAdmin.html')
+
+
+@app.route('/loginAdmAuthenticator', methods=['POST'])
+def loginAdmAuthenticator():
+    # Pegando os dados do form
+    nome = request.form['nome']
+    senha = request.form['senha']
+
+    # aqui você pode fazer a verificação com o banco de dados real, esse só está sendo usado pq nao temos um banco de dados real
+    if nome in admin and check_password_hash(admin[nome]['senha'], senha): # verificando se o email existe e se a senha está correta
+        session['liberar_adm'] = True
+        return redirect('/adm') # Redireciona para a página inicial ou outra página após o login bem-sucedido
+    else:
+        return redirect('/loginAdmin')
+    
+@app.route('/logoutAdmin', methods=['POST'])
+def logoutAdmin():  
+    return redirect('/loginAdmin')
+
+@app.route('/adm')
+def adm():
+    if not session.get('liberar_adm'):
+        return redirect('/loginAdmin')  # Ou retornar 403
+    session.pop('liberar_adm')  # remove o acesso após uso
+    return render_template('adm.html', pedidos=pedidos)
+
+@app.route('/atualizarPedido', methods=['POST'])
+def atualizarPedido():
+    
+    id = request.form['pedido_id']
+    pedidoType = request.form['pedido_type']
+
+    if pedidoType == "upgrade":
+        for pedido in pedidos:
+            if pedido == int(id):
+                print(pedido)
+                # Verificando o status atual e atualizando ou removendo conforme necessário
+                if pedidos[pedido]["status"] == "Pendente":
+                    pedidos[pedido]["status"] = "Enviado"
+                elif pedidos[pedido]["status"] == "Enviado":
+                    pedidos[pedido]["status"] = "Finalizado"
+                elif pedidos[pedido]["status"] == "Finalizado":
+                    # Deletando o pedido do dicionário
+                    del pedidos[pedido]
+                else:
+                    print("Status inválido", pedidos[pedido]["status"])
+                break
+    elif pedidoType == "downgrade":
+        for pedido in pedidos:
+            if pedido == int(id):
+                print(pedido)
+                # Verificando o status atual e atualizando ou removendo conforme necessário
+                if pedidos[pedido]["status"] == "Enviado":
+                    pedidos[pedido]["status"] = "Pendente"
+                elif pedidos[pedido]["status"] == "Finalizado":
+                    pedidos[pedido]["status"] = "Enviado"
+                else:
+                    print("Status inválido", pedidos[pedido]["status"])
+                break
+
+    return render_template('adm.html', pedidos=pedidos)
 
 if __name__ == '__main__':
     app.run(debug=True)
